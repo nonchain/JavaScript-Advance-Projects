37a4ffcbaa1d90d12cbd7479368768a3b34cfbac
progali007 <code.progali@gmail.com>
2022-09-03 16:53:25 +0430

    Add list of data to content

--- Custom Dropdown/main.js
+++ Custom Dropdown/main.js
@@ -1,3 +1,20 @@
-fetch('./countries.json')
+const wrapper = document.querySelector('.wrapper');
+const options = document.querySelector('.options');
+const btnSelect = document.querySelector('.select__btn');
+
+btnSelect.addEventListener('click', () => {
+  wrapper.classList.toggle('active');
+})
+
+addCountry();
+
+function addCountry() {
+  fetch('./countries.json')
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then(countries => {
+    countries.forEach(country => {
+      let li = `<li>${country.name}</li>`
+      options.insertAdjacentHTML("beforeend", li);
+    });
+  })
+}
