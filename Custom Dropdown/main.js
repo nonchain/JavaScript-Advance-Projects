2b534ca9a42f4ffa0301fba5e780105af5cb61df
progali007 <code.progali@gmail.com>
2022-09-27 13:56:10 +0330

    Add Serch functionality

--- Custom Dropdown/main.js
+++ Custom Dropdown/main.js
@@ -1,20 +1,44 @@
 const wrapper = document.querySelector('.wrapper');
 const options = document.querySelector('.options');
 const btnSelect = document.querySelector('.select__btn');
+const searchBar = document.querySelector('.search > input');
+const countries = [];
 
 btnSelect.addEventListener('click', () => {
   wrapper.classList.toggle('active');
 })
 
-addCountry();
+fetchData();
 
-function addCountry() {
-  fetch('./countries.json')
-  .then(response => response.json())
-  .then(countries => {
-    countries.forEach(country => {
-      let li = `<li>${country.name}</li>`
-      options.insertAdjacentHTML("beforeend", li);
-    });
-  })
+
+async function fetchData() {
+  await fetch('./countries.json')
+    .then(response => response.json())
+    .then(data => {countries.push(...data); addCountries(countries);})
 }
+
+function addCountries(countries) {
+  countries.forEach(country => {
+    let li = `<li onclick="chooseItem(this)">${country.name}</li>`
+    options.insertAdjacentHTML("beforeend", li);
+  });
+}
+
+function chooseItem(selectedItem) {
+  btnSelect.firstElementChild.innerText = selectedItem.innerText;
+  // Reset list
+  wrapper.classList.remove('active');
+  searchBar.value = "";
+  options.innerHTML = '';
+  addCountries(countries);
+}
+
+searchBar.addEventListener('input', event => {
+  const newData = countries.filter(country => {
+    return country.name.toLowerCase().startsWith(event.target.value.toLowerCase());
+  }).map(country => {
+    return `<li onclick="chooseItem(this)">${country.name}</li>`;
+  }).join('')
+
+  options.innerHTML = newData ? newData : `<p>Oops! country not found</p>`;
+});
